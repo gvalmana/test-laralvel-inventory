1b299f48aa30f3d603e5c3e744647816b027c69b
Gustavo Valmaña Villalonga <gustavo.shotokai@gmail.com>
2022-11-13 17:21:33 -0500

    creados observadores de eventos

---
+++ app/Observers/EntradaObserver.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Entrada;
+
+class EntradaObserver
+{
+    /**
+     * Handle the Entrada "created" event.
+     *
+     * @param  \App\Models\Entrada  $entrada
+     * @return void
+     */
+    public function created(Entrada $entrada)
+    {
+        //
+    }
+
+    /**
+     * Handle the Entrada "updated" event.
+     *
+     * @param  \App\Models\Entrada  $entrada
+     * @return void
+     */
+    public function updated(Entrada $entrada)
+    {
+        //
+    }
+
+    /**
+     * Handle the Entrada "deleted" event.
+     *
+     * @param  \App\Models\Entrada  $entrada
+     * @return void
+     */
+    public function deleted(Entrada $entrada)
+    {
+        //
+    }
+
+    /**
+     * Handle the Entrada "restored" event.
+     *
+     * @param  \App\Models\Entrada  $entrada
+     * @return void
+     */
+    public function restored(Entrada $entrada)
+    {
+        //
+    }
+
+    /**
+     * Handle the Entrada "force deleted" event.
+     *
+     * @param  \App\Models\Entrada  $entrada
+     * @return void
+     */
+    public function forceDeleted(Entrada $entrada)
+    {
+        //
+    }
+}
